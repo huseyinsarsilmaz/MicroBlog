fdc0fbe80d7fdafec243c794af02173842f2216d
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-01-13 15:25:42 +0300

    Fix email username typo

--- backend/src/main/java/com/hsynsarsilmaz/microblog/repository/UserRepository.java
+++ backend/src/main/java/com/hsynsarsilmaz/microblog/repository/UserRepository.java
@@ -9,5 +9,5 @@ import com.hsynsarsilmaz.microblog.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
